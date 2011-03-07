ae0de51aa616ebccd564df527fff84857feb0149
Flow86 <floriand70@gmail.com>
2011-03-07 20:57:32 +0100

    initial

---
+++ mod_oretnt.java
@@ -0,0 +1,18 @@
+public class mod_oretnt extends BaseMod
+{
+	public mod_oretnt()
+	{
+		qk.m[14].b(2000.0F);
+		qk.m[15].b(2000.0F);
+		qk.m[16].b(2000.0F);
+		qk.m[21].b(2000.0F);
+		qk.m[56].b(2000.0F);
+		qk.m[73].b(2000.0F);
+		qk.m[74].b(2000.0F);
+	}
+
+	public String Version()
+	{
+		return "1.3_01";
+	}
+}
