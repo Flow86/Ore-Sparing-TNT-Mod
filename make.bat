8c68f5987f78dfa5d79a95b8198afadc1b0f858b
Flow86 <floriand70@gmail.com>
2011-04-19 16:57:18 +0200

    updated for 1.4_01

---
+++ make.bat
@@ -0,0 +1,2 @@
+"c:\Program Files (x86)\Java\jdk1.6.0_24\bin\javac.exe" -classpath %APPDATA%\.minecraft\bin\minecraft.jar mod_oretnt.java
+pause
--- mod_oretnt.java
+++ mod_oretnt.java
@@ -2,17 +2,17 @@ public class mod_oretnt extends BaseMod
 {
 	public mod_oretnt()
 	{
-		qk.m[14].b(2000.0F);
-		qk.m[15].b(2000.0F);
-		qk.m[16].b(2000.0F);
-		qk.m[21].b(2000.0F);
-		qk.m[56].b(2000.0F);
-		qk.m[73].b(2000.0F);
-		qk.m[74].b(2000.0F);
+		ra.m[14].b(2000.0F);
+		ra.m[15].b(2000.0F);
+		ra.m[16].b(2000.0F);
+		ra.m[21].b(2000.0F);
+		ra.m[56].b(2000.0F);
+		ra.m[73].b(2000.0F);
+		ra.m[74].b(2000.0F);
 	}
 
 	public String Version()
 	{
-		return "1.3_01";
+		return "1.4_01";
 	}
 }
