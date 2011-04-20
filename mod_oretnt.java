76f69c4b6dab8eff3395eae2a8f757b8391eac43
Flow86 <floriand70@gmail.com>
2011-04-20 11:25:35 +0200

    update for 1.5

--- mod_oretnt.java
+++ mod_oretnt.java
@@ -2,17 +2,17 @@ public class mod_oretnt extends BaseMod
 {
 	public mod_oretnt()
 	{
-		ra.m[14].b(2000.0F);
-		ra.m[15].b(2000.0F);
-		ra.m[16].b(2000.0F);
-		ra.m[21].b(2000.0F);
-		ra.m[56].b(2000.0F);
-		ra.m[73].b(2000.0F);
-		ra.m[74].b(2000.0F);
+		to.m[14].b(2000.0F);
+		to.m[15].b(2000.0F);
+		to.m[16].b(2000.0F);
+		to.m[21].b(2000.0F);
+		to.m[56].b(2000.0F);
+		to.m[73].b(2000.0F);
+		to.m[74].b(2000.0F);
 	}
 
 	public String Version()
 	{
-		return "1.4_01";
+		return "1.5";
 	}
 }
